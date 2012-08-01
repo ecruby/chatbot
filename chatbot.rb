2f30979e558038a59540fea588d4192c37127168
Nick LaMuro <nicklamuro@gmail.com>
2012-07-31 22:39:37 -0500

    Adds more modular style for loading plugins

--- chatbot.rb
+++ chatbot.rb
@@ -9,6 +9,20 @@ rescue
   puts "create bot.yml and populate it with values. See the readme file!"
 end
 
+# This method is taken from rails core
+# (didn't want to load the entire lib for one method)
+# http://api.rubyonrails.org/classes/ActiveSupport/Inflector.html#method-i-constantize
+def constantize(camel_cased_word)
+  names = camel_cased_word.split('::')
+  names.shift if names.empty? || names.first.empty?
+
+  constant = Object
+  names.each do |name|
+    constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+  end
+  constant
+end
+
 $help_messages = []
 
 require './plugins/karma'
@@ -21,7 +35,7 @@ require './plugins/repeater'
     c.server = "irc.freenode.org"
     c.nick = $settings["settings"]["nick"]
     c.channels = [$settings["settings"]["channel"]]
-    c.plugins.plugins = [Karma, LinkCatcher, Repeater, Cinch::Plugins::Identify]
+    c.plugins.plugins = $settings["settings"]["plugins"].collect {|plugin| constantize(plugin)}
     c.plugins.options[Cinch::Plugins::Identify] = {
       :username => $settings['settings']['nick'],
       :password => $settings['settings']['nickserv_pass'],
